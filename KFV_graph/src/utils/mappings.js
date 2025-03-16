1e92c425691decce82378623a775448340dc8553
Maria Feher-Lehrner <maria@feher.at>
2025-03-17 00:39:52 +0100

    DB-Setup: Started implementing db mapping with js object

---
+++ KFV_graph/src/utils/mappings.js
@@ -0,0 +1,25 @@
+export const mappings = {
+  Bundesland_ID: {
+    "1": "Burgenland", "2": "Kärnten", "3": "Niederösterreich", "4": "Oberösterreich", "5": "Salzburg", "6": "Steiermark",
+    "7": "Tirol", "8": "Vorarlberg", "9": "Wien"
+  },
+  Verkehrsart_ID: {
+    "1": "Fußgänger", "2": "Fahrrad", "3": "Einspurige Kfz", "4": "Pkw", "5": "Lkw", "99": "Sonstige"
+  },
+  Geschlecht_ID: {
+    "1": "männlich", "2": "weiblich"
+  },
+  Ursache_ID: {
+    "-1": "ohne Angabe", "1": "Nichtangepasste Geschw.", "2": "Rotlichtmissachtung", "3": "Überholen", "4": "Ablenkung", "5": "Drogen", "6": "Übermüdung",
+    "7": "Fehlverh. Fußg.", "8": "Sonstige", "9": "Sonstige", "10": "Sonstige", "11": "Sonstige", "12": "Sonstige"
+  },
+  Monat_ID: {
+    "1": "Januar", "2": "Februar", "3": "März", "4": "April", "5": "Mai", "6": "Juni",
+    "7": "Juli", "8": "August", "9": "September", "10": "Oktober", "11": "November", "12": "Dezember"
+  },
+  Wochentag_ID: {
+    "1": "Montag", "2": "Dienstag", "3": "Mittwoch", "4": "Donnerstag",
+    "5": "Freitag", "6": "Samstag", "7": "Sonntag"
+  },
+
+};
