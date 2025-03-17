dd15c5ccef97801828de25ed284959e9f99b86bc
Maria Feher-Lehrner <maria@feher.at>
2025-03-17 16:49:15 +0100

    Static layout: added footer component

---
+++ KFV_graph/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup>
+export default {
+  name: "Footer"
+}
+</script>
+
+<template>
+  <div class="footer-container">
+    <p>©2025 <a href="https://github.com/Maria-Feher-Lehrner/Verkehrsdaten-Graphik" target="_blank">Maria Feher-Lehrner</a></p>
+  </div>
+
+</template>
+
+<style scoped>
+.footer-container {
+  padding: 0.5rem;
+}
+</style>
