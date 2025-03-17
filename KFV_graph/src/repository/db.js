ddeb8208f53b8707f488809e6e1d306982249a55
Maria Feher-Lehrner <maria@feher.at>
2025-03-17 18:15:15 +0100

    Database:  prepared queries in db.js

--- KFV_graph/src/repository/db.js
+++ KFV_graph/src/repository/db.js
@@ -5,3 +5,24 @@ export const db = new Dexie("TrafficAccidentDB");
 db.version(1).stores({
   accidents: "++id, jahr, monat, stunde, wochentag, bundesland, gebiet, verkehrsart, alterGr, geschlecht, ursache, getoetete"
 });
+
+//DB QUERIES
+export async function getAllAccidents() {
+  return await db.accidents.toArray();
+}
+
+export async function getAccidentsByYear(year) {
+  return await db.accidents.where("jahr").equals(year).toArray();
+}
+
+export async function getAccidentsByBundesland(bundesland) {
+  return await db.accidents.where("bundesland").equals(bundesland).toArray();
+}
+
+export async function getAccidentsByVerkehrsart(verkehrsart) {
+  return await db.accidents.where("verkehrsart").equals(verkehrsart).toArray();
+}
+
+export async function getAccidentsByGebiet(gebiet) {
+  return await db.accidents.where("gebiet").equals(gebiet).toArray();
+}
