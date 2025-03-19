51d3bddb4790255bb054b958b4f1866fb45c6cc9
Maria Feher-Lehrner <maria@feher.at>
2025-03-19 13:11:58 +0100

    State management: added data import state to uiStore

--- KFV_graph/src/stores/uiStore.js
+++ KFV_graph/src/stores/uiStore.js
@@ -2,11 +2,15 @@ import { defineStore } from "pinia";
 
 export const useUIStore = defineStore("uiStore", {
   state: () => ({
+    importComplete: false,
     chartType: "stackedArea", // Default chart type
   }),
   actions: {
     toggleChart() {
       this.chartType = this.chartType === "stackedArea" ? "bar" : "stackedArea";
+    },
+    setImportComplete(value) {
+      this.importComplete = value;
     }
   }
 });
