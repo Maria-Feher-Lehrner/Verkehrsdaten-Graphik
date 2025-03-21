1f6b31cf80e87c2461d0fbac787d4b016748f67f
Maria Feher-Lehrner <maria@feher.at>
2025-03-21 17:42:00 +0100

    Database: deleted unnecessary query preparations

--- KFV_graph/src/repository/db.js
+++ KFV_graph/src/repository/db.js
@@ -1,28 +1,7 @@
-import Dexie from "dexie";
+import Dexie from "dexie"
 
 export const db = new Dexie("TrafficAccidentDB");
 
 db.version(1).stores({
   accidents: "++id, jahr, monat, stunde, wochentag, bundesland, gebiet, verkehrsart, alterGr, geschlecht, ursache, getoetete"
 });
-
-//DB QUERIES
-export async function getAllAccidents() {
-  return await db.accidents.toArray();
-}
-
-export async function getAccidentsByYear(year) {
-  return await db.accidents.where("jahr").equals(year).toArray();
-}
-
-export async function getAccidentsByBundesland(bundesland) {
-  return await db.accidents.where("bundesland").equals(bundesland).toArray();
-}
-
-export async function getAccidentsByVerkehrsart(verkehrsart) {
-  return await db.accidents.where("verkehrsart").equals(verkehrsart).toArray();
-}
-
-export async function getAccidentsByGebiet(gebiet) {
-  return await db.accidents.where("gebiet").equals(gebiet).toArray();
-}
